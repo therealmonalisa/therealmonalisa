00484b942659a1405bf2904381c62604680abb3f
therealmonalisa <31523366+therealmonalisa@users.noreply.github.com>
2017-12-14 18:15:51 -0500

    reupload test

---
+++ main.js
@@ -0,0 +1,40 @@
+var hello="hello world";
+console.log("hello")
+
+// alert("Davinci Made Me! The Internet Raised Me!");
+
+
+function myButton(){
+var txt 
+var person = prompt("Please enter your email", "monalisa@monalisa.com");
+
+if (person == null || person == "") {
+    txt = "Invalid Email Address";
+} else {
+    txt = "Hello! Thank you for registering for the Mona Lisa Alerts";
+}}
+
+
+
+
+
+
+var buttonTest=document.getElementById("wtf");
+
+buttonTest.addEventListener("click",clickFunction);
+
+$('#imageid').hover(function() {
+  $(this).attr('src', '/folder/image2.jpg');
+}, function() {
+  $(this).attr('src', '/folder/image1.jpg');
+});
+
+// function clickFunction (){
+//  document.getElementById("button-test").innerHTML="testing bitches hello";
+// }
+//ONE
+
+
+
+//TWO
+
